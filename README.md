683374e5faf2db0e9d27c8b2befbf62ff87424c1
Alex Lutz <alexinslc@gmail.com>
2022-04-22 19:38:16 -0600

    initial commit with server code and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # http_server_ruby
+
+NOTE: This thing isn't secure.
+
+It's just for demo purposes.
+
+Don't use this in production.
+
+It's not even close to secure.
+
+Why is it not secure?
+1. Run `ruby server.rb` 
+2. Run `curl -v http://localhost:2000/../server.rb --path-as-is`
+
+The above command will literally return the contents of server.rb - that's bad. 
---
+++ server.rb
@@ -0,0 +1,87 @@
+# NOTE: This thing isn't secure.
+# It's just for demo purposes.
+# Don't use this in production.
+# It's not even close to secure.
+# See why: curl -v http://localhost:2000/../server.rb --path-as-is 
+# The above command will literally return the contents of server.rb - that's bad. 
+
+require "socket"
+require "active_support/all"
+
+port = ENV.fetch("PORT") { 2000 }.to_i
+server = TCPServer.new(2000)
+
+puts "Listening on port #{port}..."
+
+class Request
+  attr_reader :method, :path, :headers, :body
+
+  def initialize(request)
+    lines = request.lines
+    index = lines.index ("\r\n")
+
+    @method, @path, _ = lines.first.split
+    @path, @query = @path.split("?")
+    @headers = parse_headers(lines[1...index])
+    @body = lines[index + 1..-1].join
+  end
+  
+  def parse_headers(lines)
+    headers = {}
+    lines.each do |line|
+      name, value = line.split(": ")
+      headers[name] = value.chomp
+    end
+
+    headers
+  end
+
+  def content_length
+    headers["Content-Length"].to_i
+  end
+end
+
+class Response
+  def initialize(code:, body: "")
+    @code = code
+    @body = body
+  end
+
+  def send(client)
+    client.print "HTTP/1.1 #{@code} \r\n"
+    client.print "Content-Type: text/html\r\n"
+    client.print "Content-Length: #{@body.length}\r\n"
+    client.print "\r\n"
+    client.print "#{@body}\r\n" if @body.present?
+  end
+end
+
+def render(path)
+  full_path = File.join(__dir__, "views", path)
+  if File.exists?(full_path)
+    Response.new(code: 200, body: File.binread(full_path))
+  else
+    Response.new(code: 404, body: "File Not Found")
+  end
+end
+
+def route(request)
+  if request.path == "/"
+    render "index.html"
+  else
+    render request.path
+  end
+end
+
+
+loop do
+  Thread.start(server.accept) do |client|
+    request = Request.new client.readpartial(2048)
+    
+    response = route(request)
+
+    response.send(client)
+    
+    client.close
+  end
+end
